bf54b330621d6c5daad5a3bc76cb8bfe7a69eade
dgzhuya <chenbiuxiu@gmail.com>
2024-06-12 23:04:58 +0800

    ✨ feat: 完成后端API模块生成

--- src/codegen.ts
+++ src/codegen.ts
@@ -3,34 +3,71 @@ import { join } from 'path'
 import { writeFormatFile } from './util'
 import { existsSync, mkdirSync } from 'fs'
 
-export const genDtoCode = async (path = '', moduleName = '') => {
-	const dtoPath = join(path, 'dto')
-	if (!existsSync(dtoPath)) {
-		mkdirSync(dtoPath)
+export class GenApi {
+	#path: string
+	#name: string
+	constructor(path: string, name: string) {
+		this.#name = name
+		this.#path = path
 	}
-	const createDtoPath = join(dtoPath, `create-${moduleName}.dto.ts`)
-	const updateDtoPath = join(dtoPath, `update-${moduleName}.dto.ts`)
-	const createDtoStr = await xiu.render('create-dto', {
-		importInfo:
-			"import { IsNotEmpty, IsOptional, Length, MaxLength } from 'class-validator'\n",
-		name: moduleName,
-		content: 'name:string'
-	})
-	const updateDtoStr = await xiu.render('update-dto', {
-		name: moduleName
-	})
-	writeFormatFile(createDtoPath, createDtoStr)
-	writeFormatFile(updateDtoPath, updateDtoStr)
-}
 
-export const genEntity = async (path = '', moduleName = '') => {
-	const entityDir = join(path, 'entities')
-	if (!existsSync(entityDir)) {
-		mkdirSync(entityDir)
+	gen() {
+		this.#genDto()
+		this.#genEntity()
+		this.#genController()
+		this.#genMoudle()
+		this.#genService()
 	}
-	const entityPath = join(entityDir, `${moduleName}.entity.ts`)
-	const entityStr = await xiu.render('entity', {
-		name: moduleName
-	})
-	writeFormatFile(entityPath, entityStr)
+
+	async #genService() {
+		const servicePath = join(this.#path, `${this.#name}.service.ts`)
+		const serviceStr = await xiu.render('service', { name: this.#name })
+		writeFormatFile(servicePath, serviceStr)
+	}
+
+	async #genMoudle() {
+		const modulePath = join(this.#path, `${this.#name}.module.ts`)
+		const moduleStr = await xiu.render('module', { name: this.#name })
+		writeFormatFile(modulePath, moduleStr)
+	}
+
+	async #genController() {
+		const controllerPath = join(this.#path, `${this.#name}.controller.ts`)
+		const controllerStr = await xiu.render('controller', {
+			name: this.#name
+		})
+		writeFormatFile(controllerPath, controllerStr)
+	}
+
+	async #genEntity() {
+		const entityDir = join(this.#path, 'entities')
+		if (!existsSync(entityDir)) {
+			mkdirSync(entityDir)
+		}
+		const entityPath = join(entityDir, `${this.#name}.entity.ts`)
+		const entityStr = await xiu.render('entity', {
+			name: this.#name
+		})
+		writeFormatFile(entityPath, entityStr)
+	}
+
+	async #genDto() {
+		const dtoPath = join(this.#path, 'dto')
+		if (!existsSync(dtoPath)) {
+			mkdirSync(dtoPath)
+		}
+		const createDtoPath = join(dtoPath, `create-${this.#name}.dto.ts`)
+		const updateDtoPath = join(dtoPath, `update-${this.#name}.dto.ts`)
+		const createDtoStr = await xiu.render('create-dto', {
+			importInfo:
+				"import { IsNotEmpty, IsOptional, Length, MaxLength } from 'class-validator'\n",
+			name: this.#name,
+			content: 'name:string'
+		})
+		const updateDtoStr = await xiu.render('update-dto', {
+			name: this.#name
+		})
+		writeFormatFile(createDtoPath, createDtoStr)
+		writeFormatFile(updateDtoPath, updateDtoStr)
+	}
 }
--- src/libs.ts
+++ src/libs.ts
@@ -1,15 +1,14 @@
 import { join } from 'path'
 import { getApiDir } from './config'
 import { existsSync, mkdirSync } from 'fs'
-import { genDtoCode, genEntity } from './codegen'
+import { GenApi } from './codegen'
 
 export function genApiCode(name: string) {
 	const basePath = join(getApiDir(), name)
 	if (!existsSync(basePath)) {
 		mkdirSync(basePath)
 	}
-	genDtoCode(basePath, name)
-	genEntity(basePath, name)
+	new GenApi(basePath, name).gen()
 }
 
 export function genWebCode(name: string, val: any) {
