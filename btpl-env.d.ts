1f3659285ea06ad41f958820e7195857a2d2770a
dgzhuya <chenbiuxiu@gmail.com>
2024-06-12 20:36:24 +0800

    ✨ feat: 添加entity代码生成

--- btpl-env.d.ts
+++ btpl-env.d.ts
@@ -3,15 +3,21 @@ import '@biuxiu/template'
 declare module '@biuxiu/template' {
 	interface TemplateMap {
 		'create-dto': CreateDto
+		entity: Entity
 		'update-dto': UpdateDto
 	}
 
 	interface CreateDto {
 		name: string
 		content: string
+		import_info: string
 	}
 
 	interface UpdateDto {
+		name: string
+	}
+
+	interface Entity {
 		name: string
 	}
 }
