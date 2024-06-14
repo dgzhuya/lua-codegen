13ebf7058194f45be4c4803593009c513804cab4
dgzhuya <chenbiuxiu@gmail.com>
2024-06-14 15:51:48 +0800

    ✨ feat: 添加规则创建函数

--- scripts/lua/meta/node-types.lua
+++ scripts/lua/meta/node-types.lua
@@ -23,10 +23,38 @@ NodeJs = {}
 --- @param name string 模块名,使用`.`分隔上下级模块
 function NodeJs.genWebCode(route, tbl, form, name) end
 
+--- 打印输出
+function logger(...) end
+
+--- 生成限制规则
+---
+--- @param max number|false 最大值
+--- @param min number|false  最小值
+--- @param msg string 消息提示
+--- @return table
+function NodeJs.createLimitRule(max, min, msg) end
+
+--- 生成简单规则
+---
+---@param key 'isOptional'|'notEmpty'|'isInt'|'isNumber' 规则类型
+---@param msg string|true 消息提示
+function NodeJs.createSimpleRule(key, msg) end
+
+--- @class DTORule
+
+--- 生成DTO校验规则
+---
+---@param ... function 生成规则函数
+---@return DTORule
+function NodeJs.createDtoRule(...) end
+
+--- @class DTOSchema
+--- @field key string
+--- @field type 'string' | 'number' | 'bool'
+--- @field rule DTORule
+
 --- 创建后端代码
 ---
 --- @param name string 模块名
-function NodeJs.genApiCode(name) end
-
---- 打印输出
-function logger(...) end
+--- @param dto  DTOSchema[]  dto数据结构
+function NodeJs.genApiCode(name, dto) end
