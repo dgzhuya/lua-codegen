33349f0f0d680ab48b099d43213221aa9badf571
dgzhuya <chenbiuxiu@gmail.com>
2024-06-14 15:55:28 +0800

    🧪 test: 修改测试用例

--- scripts/lua/test.lua
+++ scripts/lua/test.lua
@@ -1,1 +1,10 @@
-NodeJs.genApiCode('student')
+NodeJs.genApiCode('student', {
+    {
+        key = 'name',
+        type = 'string',
+        rule = NodeJs.createDtoRule(
+            NodeJs.createSimpleRule('isOptional', true),
+            NodeJs.createLimitRule(false, 10, "最小长度为10")
+        )
+    }
+})
