ac36f9ce201cd4cc85dde8a88400be1c3aeba1d1
dgzhuya <chenbiuxiu@gmail.com>
2024-06-14 15:53:26 +0800

    🦄 refactor: 优化代码生成流程

--- src/libs.ts
+++ src/libs.ts
@@ -2,13 +2,19 @@ import { join } from 'path'
 import { getApiDir } from './config'
 import { existsSync, mkdirSync } from 'fs'
 import { GenApi } from './codegen'
+import { DTOSchema } from './types'
 
-export function genApiCode(name: string) {
+export function genApiCode(name: string, dto: DTOSchema[]) {
 	const basePath = join(getApiDir(), name)
 	if (!existsSync(basePath)) {
 		mkdirSync(basePath)
 	}
-	new GenApi(basePath, name).gen()
+	const api = new GenApi(basePath, name)
+	api.genModule()
+	api.genDto(dto)
+	api.genEntity()
+	api.genService()
+	api.genController()
 }
 
 export function genWebCode(name: string, val: any) {
