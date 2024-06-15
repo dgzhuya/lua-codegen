9369ece641643aacf8e3d86111b5d1aca2d7a3d8
dgzhuya <chenbiuxiu@gmail.com>
2024-06-16 01:05:12 +0800

    🦄 refactor: 修改Dto类型命名

--- src/libs/nest-lib.ts
+++ src/libs/nest-lib.ts
@@ -2,9 +2,9 @@ import { join } from 'path'
 import { getApiDir } from '@/config'
 import { existsSync, mkdirSync } from 'fs'
 import { GenApi } from '@/code/nest'
-import { DTOSchema } from '@/types'
+import { DtoField } from '@/types'
 
-export function genApiCode(name: string, dto: DTOSchema[]) {
+export function genApiCode(name: string, dto: DtoField[]) {
 	const basePath = join(getApiDir(), name)
 	if (!existsSync(basePath)) {
 		mkdirSync(basePath)
