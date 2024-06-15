b97347a199d8284870cdd15d49ee27b623da0eb3
dgzhuya <chenbiuxiu@gmail.com>
2024-06-16 01:04:31 +0800

    🦄 refactor: nil参数bug修复，不需要none代替

--- lua/meta/base-type.lua
+++ lua/meta/base-type.lua
@@ -1,7 +1,4 @@
 --- @meta Base
 
---- @alias None None
-None = None
-
 --- 打印输出
 function logger(...) end
--- src-rust/lib.rs
+++ src-rust/lib.rs
@@ -3,7 +3,7 @@ mod vuejs_lib;
 
 use gloo_utils::format::JsValueSerdeExt;
 use neige_lua::{
-    api::{CallApi, LuaApi, PushApi, SetApi},
+    api::{CallApi, LuaApi, PushApi},
     state::LuaState,
     LuaValue,
 };
@@ -37,8 +37,6 @@ pub fn run(data: &[u8], file_name: &str) {
     let mut state = LuaState::new();
     state.aux_lib();
     state.register("logger", logger);
-    state.push_nil();
-    state.set_global("None");
     state.nestjs_lib();
     state.vuejs_lib();
     state.load(data.to_vec(), file_name, "bt");
