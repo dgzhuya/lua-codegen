69cb94be6cf1a9eeac0ec39f6a67da79363b0657
dgzhuya <chenbiuxiu@gmail.com>
2024-06-18 13:22:04 +0800

    🧪 test: 优化测试脚本

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,7 +1,9 @@
 import { startRun } from '../src/index'
 import { resolve } from 'path'
 
+const isDelete = !!process.argv.slice(2)[0]
+
 const luaPath = resolve(__dirname, '../lua/tests/gen_api.lua')
 startRun(luaPath, {
-	apiDir: '/Users/pinktu/Desktop/develop/wink-project/wink-vue-admin/packages/api/src'
+	reverse: isDelete
 })
