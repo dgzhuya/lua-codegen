398938d37e8ab978b00202d716cd7fe59877b473
dgzhuya <chenbiuxiu@gmail.com>
2024-06-20 11:50:50 +0800

    🧪 test: 修改测试脚本

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,9 +1,11 @@
-import { startRun } from '../src/index'
+import { startGen, startRemove } from '../src/index'
 import { resolve } from 'path'
 
 const isDelete = !!process.argv.slice(2)[0]
 
 const luaPath = resolve(__dirname, '../lua/tests/gen_api.lua')
-startRun(luaPath, {
-	reverse: isDelete
-})
+if (isDelete) {
+	startRemove(luaPath)
+} else {
+	startGen(luaPath)
+}
